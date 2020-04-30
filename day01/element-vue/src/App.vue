f5aaef8f850e037ecdd6f75e29fe66598e8097e7
lanweipeng <1462191066@qq.com>
2020-04-30 21:27:52 +0800

    day-domo

---
+++ day01/element-vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  vue
+  <nav>
+    <div>外卖</div>
+    <div>生活</div>
+    <div>订单</div>
+    <div>我的</div>
+  </nav>
+</div>
+</template>
+<script>
+export default {
+data(){
+return {
+}
+},created(){},methods:{},components:{}
+}
+</script>
+<style lang='less' scoped>
+nav{
+  position: fixed;
+  bottom: 0;
+}
+</style>
---
+++ day01/element-vue/src/route/index.js
@@ -0,0 +1,2 @@
+import Vue from 'vue'
+import Route from 'vue-router'
